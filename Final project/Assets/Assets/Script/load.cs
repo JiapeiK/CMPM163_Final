15d160ef3c6d1179a1c59da72dccc03831cee40b
Jiapei Kuang <jkuang6@ucsc.edu>
2019-06-13 19:31:54 -0700

    First Draft

---
+++ Final project/Assets/Assets/Script/load.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class load : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            SceneManager.LoadScene("Daylight");
+       }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            SceneManager.LoadScene("rain");
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            SceneManager.LoadScene("Starlight");
+        }
+        if (Input.GetKeyUp(KeyCode.F))
+        {
+            SceneManager.LoadScene("Shooting star");
+        }
+        if (Input.GetKeyUp(KeyCode.G))
+        {
+            SceneManager.LoadScene("Firework");
+        }
+    }
+}
